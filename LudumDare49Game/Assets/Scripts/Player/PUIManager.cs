363c4b91ce1b9d90ac44bed0ebb4e57c3018fc98
ethanKessler.dev <ethankessler.dev@gmail.com>
2021-10-02 22:35:35 +0100

    Most of the gameplay done

---
+++ LudumDare49Game/Assets/Scripts/Player/PUIManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PUIManager : MonoBehaviour
+{
+    public Image shroud;
+    public GameObject endScreen;
+    public GameObject score;
+
+    /// <summary>
+    /// Fade in shroud that covers player screen
+    /// </summary>
+    /// <param name="fadeAway"></param>
+    /// <returns></returns>
+    public IEnumerator FadeShroud(bool fadeAway)
+    {
+        if (fadeAway)
+        {
+            for (float i = 1; i >= 0; i -= Time.deltaTime)
+            {
+                shroud.color = new Color(0, 0, 0, i);
+                yield return null;
+            }
+            
+        }
+        else
+        {
+            for (float i = 0; i <= 1; i += Time.deltaTime)
+            {
+                shroud.color = new Color(0, 0, 0, i);
+                yield return null;
+            }
+        }
+        shroud.gameObject.SetActive(!fadeAway);
+    }
+
+    void Start()
+    {
+        StartCoroutine(FadeShroud(true));
+        Debug.Log("Fading");
+    }
+
+    public void gameEndScreen()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Debug.Log("Something is meant to be happening");
+        Boat theBoat = GameObject.FindObjectOfType<Boat>();
+        float distanceTravelled = theBoat.transform.position.z;
+        score.GetComponent<TextMeshProUGUI>().SetText($"You travelled {distanceTravelled} metres.");
+        endScreen.SetActive(true);
+    }
+}
--- LudumDare49Game/Assets/Scripts/VoidController.cs
+++ LudumDare49Game/Assets/Scripts/VoidController.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class VoidController : MonoBehaviour
 {
+    bool gameOver = false;
     public void GameEnd()
     {
-        Time.timeScale = 0;
+        if(!gameOver)
+        {
+            Time.timeScale = 0;
+            PUIManager pUIManager = GameObject.FindObjectOfType<PUIManager>();
+            StartCoroutine(pUIManager.FadeShroud(false));
+            pUIManager.gameEndScreen();
+            StartCoroutine(pUIManager.FadeShroud(true));
+            gameOver = true;
+        }
     }
 }
